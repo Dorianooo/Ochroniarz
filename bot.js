0f74390c1a529349ebaff0775525dee9192443b3
Dorianooo <40131357+Dorianooo@users.noreply.github.com>
2018-06-10 04:02:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(NDU1MTg2ODA0MDM2MDc1NTMw.Df4VVw.8WvbDSupOy7pgrR0UG6zO_w3ZA0);
+client.login(process.env.BOT_TOKEN);
